b4c374f5e59bb7be6148d42b5ade1659d8f60c15
Johnny Lord <johnlord01@gmail.com>
2026-06-21 00:17:49 -0400

    Updated Astro config and installed React

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://practicalpodiatrist.com',
+  integrations: [mdx(), react(), sitemap()],
+  vite: { plugins: [tailwindcss()] }
+});
